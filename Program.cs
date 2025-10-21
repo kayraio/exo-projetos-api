6e8d9c21d7c0c16be84d73db17f36b071a9b7d0f
kayraio <kayra.6@hotmail.com>
2025-10-21 13:09:33 -0300

    Adiciona arquivos da API (Program.cs, appsettings, context, models, controllers)

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona os controladores
+builder.Services.AddControllers();
+
+// Adiciona o Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configura o DbContext com SQL Server
+builder.Services.AddDbContext<ExoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Registra os repositórios
+builder.Services.AddScoped<ProjetoRepository>();
+builder.Services.AddScoped<UsuarioRepository>();
+
+var app = builder.Build();
+
+// Configuração do ambiente
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
